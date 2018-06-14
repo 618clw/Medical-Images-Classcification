fbfa8e016b69218b0bf9128c15acd3ac3e4c7eed
wangboyu15 <34835504+wangboyu15@users.noreply.github.com>
2018-06-14 15:18:31 +0800

    图形界面(原始图片+热图+分类结果)

--- LetsPlay.py
+++ LetsPlay.py
@@ -1,8 +1,28 @@
 import tkinter as tk
 import PIL
 from PIL import Image
-#from PIL import ImageTK
+import imageio
+# from PIL import ImageTK
 window = tk.Tk()
+
+def create_gif(image_list, gif_name):
+    frames = []
+    for image_name in image_list:
+        frames.append(imageio.imread(image_name))
+        # Save them as frames into a gif
+    imageio.mimsave(gif_name, frames, 'GIF', duration=0.1)
+
+    return
+
+def gif_heatmap():
+    image_list = ['heatmap.png']
+    gif_name = 'heatmap.gif'
+    create_gif(image_list, gif_name)
+
+def gif_iniImag(init):
+    image_list = [str(init)+'.png']
+    gif_name = 'init.gif'
+    create_gif(image_list, gif_name)
 
 def paosini():
 
@@ -38,27 +58,37 @@ def paosini():
     elif CResult == 14:
         confirmLabel.config(text="Hernia")
 
-def Showing_Imag():
+def Showing_iniImag():
     global ImagName
     ImagName = ImagEntry.get()
     print('name:')
     print(ImagName)
-    img_open = Image.open("D:/Program Files/Python/chexnet1-master/"+str(ImagName)+".png")
-    img_open.show()
-    # img_png = PIL.ImageTK.PhotoImage(img_open)
-    # label_img = tk.Label(image=img_open)
-    # label_img.pack()
+    gif_iniImag(ImagName)     # convert png to gif
+
+    img_png_1 = tk.PhotoImage(file='init.gif')
+    label_img1 = tk.Label(image=img_png_1)
+    label_img1.image = img_png_1
+    label_img1.pack(side='left')
+
+def Showing_heatmap():
+    gif_heatmap()     # convert png to gif
+    img_png_2 = tk.PhotoImage(file='heatmap.gif')
+    label_img2 = tk.Label(image=img_png_2)
+    label_img2.image = img_png_2
+    label_img2.pack(side='right')
+
 
 ImagLabel = tk.Label(window, text="ImagName: ")
 ImagEntry = tk.Entry(window)
 ImagName = ImagEntry.get()
 
-button1 = tk.Button(window, text="Show Picture", command=Showing_Imag)
+button1 = tk.Button(window, text="Show iniPicture", command=Showing_iniImag)
 # img_open = Image.open(ImagName)
 # img_png = ImageTK.PhotoImage(img_open)
 # label_img = tk.Label(root, image=img_png)
 
 button2 = tk.Button(window, text="Run", command=paosini)
+button3 = tk.Button(window, text="Show Heatmap", command=Showing_heatmap)
 confirmLabel = tk.Label(window)
 
 ImagLabel.pack()
@@ -66,6 +96,7 @@ ImagEntry.pack()
 button1.pack()
 # label_img.pack()
 button2.pack()
+button3.pack()
 confirmLabel.pack()
 
 window.mainloop()
